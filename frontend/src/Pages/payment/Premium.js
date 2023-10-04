540ca65e4acb39b4a7d3aadf4da0aea30d1891db
Altafalam3 <altaf.alam0032@gmail.com>
2023-10-05 00:59:51 +0530

    paisa hi paisa

--- frontend/src/Pages/payment/Premium.js
+++ frontend/src/Pages/payment/Premium.js
@@ -1,8 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
+import { UserContext } from '../../context/UserContext';
 import "./Premium.css";
 import axios from "axios";
 
 export default function Premium() {
+    const { isLoggedIn, userr, checkUserLoggedIn } = useContext(UserContext);
+
     const gradientColors = [
         "#67E0DD",
         "#A6D8DF",
@@ -18,13 +21,14 @@ export default function Premium() {
         justifyContent: "center",
         alignItems: "center",
     };
-    const [premiumprice, setPremiumprice] = useState(199);
+    const [premiumprice, setPremiumprice] = useState(350);
     const handlepayment = async () => {
         try {
-            // if(!localStorage.getItem("token")){
-            // 	alert("You have not logged in.");
-            // 	return navigate("/login");
-            // }
+            if (!isLoggedIn) {
+                alert("You are not logged in.");
+                return;
+                // return navigate("/login");
+            }
             // console.log("Hanlde")
             const orderUrl = "http://localhost:8800/api/pay/orders";
             const { data } = await axios.post(
@@ -47,8 +51,9 @@ export default function Premium() {
             console.log(error);
         }
     };
+    
     const initPayment = (data) => {
-        console.log("In init");
+        console.log("In init")
         const options = {
             key: "rzp_test_zkRk5Km3mrtYWp",
             amount: data.amount,
@@ -58,13 +63,11 @@ export default function Premium() {
             handler: async (response) => {
                 try {
                     const verifyUrl = "http://localhost:8800/api/pay/verify";
-                    const { data } = await axios.post(verifyUrl, response, {
+                    const { data } = await axios.post(verifyUrl, { ...response, userId: userr._id }, {
                         headers: {
-                            "Access-Control-Allow-Origin": "*",
-                            "Access-Control-Allow-Methods":
-                                "POST, GET, OPTIONS, PUT, DELETE",
-                            "Access-Control-Allow-Headers":
-                                "Content-Type, X-Auth-Token, Origin, Authorization",
+                            'Access-Control-Allow-Origin': '*',
+                            'Access-Control-Allow-Methods': 'POST, GET, OPTIONS, PUT, DELETE',
+                            'Access-Control-Allow-Headers': 'Content-Type, X-Auth-Token, Origin, Authorization',
                         },
                     });
                     console.log(data);
@@ -81,9 +84,8 @@ export default function Premium() {
             },
         };
 
-        console.log(options);
+        console.log(options)
         const rzp1 = new window.Razorpay(options);
-
         rzp1.open();
     };
 
